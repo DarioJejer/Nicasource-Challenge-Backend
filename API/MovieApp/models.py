4f3a16f172bc68d93ead7d61d217b49ccd60feb6
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-11-13 06:00:17 -0300

    Added User model

--- API/MovieApp/models.py
+++ API/MovieApp/models.py
@@ -8,8 +8,17 @@ class Movies(models.Model):
     Title = models.CharField(max_length=200)
     Release = models.DateField()
     Plot = models.CharField(max_length=1000)
+    Poster = models.CharField(max_length=1000)
+
+class Users(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Username = models.CharField(max_length=100)
+    Password = models.CharField(max_length=100)
 
 class Ratings(models.Model):
     Id = models.AutoField(primary_key=True)
     Rating = models.IntegerField()
+    Date = models.DateField()
     Comment = models.CharField(max_length=500)
+    Movie = models.ForeignKey(Movies, on_delete=models.CASCADE)
+    User = models.ForeignKey(Users, on_delete=models.CASCADE)

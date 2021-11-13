98a917f2ebf44c1557a1f5bc7e4946c8148c35a5
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-11-13 06:57:37 -0300

    Added base endpoints for Users and Ratings

--- API/MovieApp/urls.py
+++ API/MovieApp/urls.py
@@ -3,5 +3,11 @@ from MovieApp import views
 
 urlpatterns=[
     url(r'^movie/$', views.movieApi),
-    url(r'^movie/([0-9]+)$',views.movieApi)
+    url(r'^movie/([0-9]+)$',views.movieApi),
+    
+    url(r'^rating/$', views.ratingApi),
+    url(r'^rating/([0-9]+)$',views.ratingApi),
+    
+    url(r'^user/$', views.userApi),
+    url(r'^user/([0-9]+)$',views.userApi)
 ]
--- API/MovieApp/views.py
+++ API/MovieApp/views.py
@@ -4,8 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 
-from MovieApp.models import Movies, Ratings
-from MovieApp.serializers import MovieSerializer, RatingSerializer
+from MovieApp.models import Movies, Ratings, Users
+from MovieApp.serializers import MovieSerializer, RatingSerializer, UserSerializer
 
 # Create your views here.
 @csrf_exempt
@@ -37,4 +37,62 @@ def movieApi(request, id=0):
         movie.delete()
         return JsonResponse("Deleted Successfully!!", safe=False)
 
+@csrf_exempt
+def ratingApi(request, id=0):
+    if request.method == 'GET':
+        ratings = Ratings.objects.all()
+        ratings_serializer = RatingSerializer(ratings, many=True)
+        return JsonResponse(ratings_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        ratings_data=JSONParser().parse(request)
+        ratings_serializer = RatingSerializer(data=ratings_data)
+        if ratings_serializer.is_valid():
+            ratings_serializer.save()
+            return JsonResponse("Added Successfully!!", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+
+    elif request.method == 'PUT':
+        ratings_data = JSONParser().parse(request)
+        ratings=Ratings.objects.get(Id=ratings_data['Id'])
+        ratings_serializer = RatingSerializer(ratings,data=ratings_data)
+        if ratings_serializer.is_valid():
+            ratings_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False)
+        return JsonResponse("Failed to Update", safe=False)
+    
+    elif request.method == 'DELETE':
+        ratings=Ratings.objects.get(Id=id)
+        ratings.delete()
+        return JsonResponse("Deleted Successfully!!", safe=False)
+
+
+@csrf_exempt
+def userApi(request, id=0):
+    if request.method == 'GET':
+        users = Users.objects.all()
+        users_serializer = UserSerializer(users, many=True)
+        return JsonResponse(users_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        users_data=JSONParser().parse(request)
+        users_serializer = UserSerializer(data=users_data)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return JsonResponse("Added Successfully!!", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+
+    elif request.method == 'PUT':
+        users_data = JSONParser().parse(request)
+        users=Users.objects.get(Id=users_data['Id'])
+        users_serializer = UserSerializer(users,data=users_data)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False)
+        return JsonResponse("Failed to Update", safe=False)
+    
+    elif request.method == 'DELETE':
+        users=Users.objects.get(Id=id)
+        users.delete()
+        return JsonResponse("Deleted Successfully!!", safe=False)
 
